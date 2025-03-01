719cd8c14bb468029d590dfa75999f284de30086
Alexander Tuzhik <alprog@hotmail.com>
2025-03-01 22:06:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PixHub
+{
+    internal class Program
+    {
+
+        static async Task<PixooDevice> Connect()
+        {
+            var client = new HttpClient();
+            string url = "https://app.divoom-gz.com/Device/ReturnSameLANDevice";
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            string responseText = await responseMessage.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { IncludeFields = true };
+            var response = JsonSerializer.Deserialize<FindDeviceResponse>(responseText, options);
+            return new PixooDevice(response.DeviceList[0]);
+        }
+
+        static async Task Main(string[] args)
+        {
+            var device = await Connect();
+
+
+            device.SetBrightness(50);
+
+            device.SendAnimation();
+        }
+
+    }
+}
---
+++ Structs.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections.Generic;
+
+namespace PixHub
+{
+    public struct DeviceInfo
+    {
+        public string DeviceName;
+        public int DeviceId;
+        public string DevicePrivateIP;
+        public string DeviceMac;
+    }
+
+    public struct FindDeviceResponse
+    {
+        public int ReturnCode;
+        public string ReturnMessage;
+        public List<DeviceInfo> DeviceList;
+    }
+
+    public struct SetBrightnessRequest
+    {
+        public string Command;
+        public int Brightness;
+    }
+
+    public struct SendAnimationRequest
+    {
+        public string Command;
+        public int PicNum;
+        public int PicWidth;
+        public int PicOffset;
+        public int PicID;
+        public int PicSpeed;
+        public string PicData;
+    }
+
+    public struct RegularResponse
+    {
+        public int error_code;
+    }
+}
